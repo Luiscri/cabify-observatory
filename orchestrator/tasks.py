63e487d843e5804ad9f8be95910e3338192d00de
Luis Cristobal Lopez Garcia <luiscri96@gmail.com>
2020-03-20 17:32:17 +0100

    Improved tasks.py readability

--- orchestrator/tasks.py
+++ orchestrator/tasks.py
@@ -3,6 +3,7 @@ sys.dont_write_bytecode = True
 
 # logging
 import logging
+import traceback
 
 # orchestration
 import luigi
@@ -34,11 +35,13 @@ class ScrapperTask(luigi.Task):
         if os.path.exists(previous_month_directory):
             subprocess.check_output(['tar','-zcf','{}.tar.gz'.format(previous_month_directory), previous_month_directory])
             shutil.rmtree(previous_month_directory, ignore_errors=True)
+            logger.info('{} directory zipped and removed.'.format(previous_month_directory), extra={'methodname': self.__class__.__name__})
 
         two_months_ago = (self.run_date-timedelta(days=60)).strftime('%Y-%m')
         two_months_ago_tar = os.path.join(os.getcwd(), 'data', '{}.tar.gz'.format(two_months_ago))
         if os.path.isfile(two_months_ago_tar):
             os.remove(two_months_ago_tar)
+            logger.info('{} file removed.'.format(two_months_ago_tar), extra={'methodname': self.__class__.__name__})
 
         query = '"movilidad sostenible" OR "transporte sostenible" OR ((contaminacion OR contaminación) AND (no2 OR "dioxido de nitrogeno" OR "dióxido de nitrógeno" OR nox))'
         lang = 'es'
@@ -48,17 +51,18 @@ class ScrapperTask(luigi.Task):
         articles = retrieveAllArticles(query, lang, yesterday)
 
         filtered = []
-        host = os.environ.get('ES_HOST')
+        host = os.environ.get('ES_NETWORK_LOCATION').split(':')[0]
+        port = os.environ.get('ES_NETWORK_LOCATION').split(':')[1]
         try:
-            es = Elasticsearch(hosts=host)
+            es = Elasticsearch(hosts=[{'host': host, 'port': port}])
             for article in articles:
                 if es.exists(index=self.index, doc_type=self.doc_type, id=article['id']):
-                    logger.info('Duplicated article found.', extra={'methodname': self.__class__.__name__})
+                    logger.info('Article discarded: id already exists on ElasticSearch index.', extra={'methodname': self.__class__.__name__})
                     continue
                 else:
                     filtered.append(article)
-        except:
-            logger.error("Error performing exists() query to ElasticSearch.", extra={'methodname': self.__class__.__name__})
+        except Exception as e:
+            logger.error("Error performing exists() query to ElasticSearch client: {}".format(traceback.format_exc()), extra={'methodname': self.__class__.__name__})
             filtered = articles
 
         if not os.path.exists(self.output_directory):
@@ -68,7 +72,7 @@ class ScrapperTask(luigi.Task):
             for article in filtered:
                 saving = json.dumps(article, ensure_ascii=False)
                 outfile.write(saving+'\n')
-            logger.info("{} articles retrieved.".format(len(filtered)), extra={'methodname': self.__class__.__name__})
+            logger.info("{} articles retrieved, {} articles discarded.".format(len(filtered), len(articles)-len(filtered)), extra={'methodname': self.__class__.__name__})
 
     def output(self):
         file_path = os.path.join(self.output_directory, '{}.txt'.format(self.run_date))
@@ -138,7 +142,15 @@ class SearchAndStoreTask(CopyToIndex):
     run_date = luigi.parameter.DateParameter()
     index = luigi.parameter.Parameter()
     doc_type = luigi.parameter.Parameter()
-    host = os.environ.get('ES_HOST')
+    host = os.environ.get('ES_NETWORK_LOCATION').split(':')[0]
+    port = os.environ.get('ES_NETWORK_LOCATION').split(':')[1]
+
+    def run(self):
+        super().run()
+
+        with self.input().open('r') as infile:
+            num_articles = sum(1 for line in infile)
+        logger.info("Uploading {} articles to ElasticSearch...".format(num_articles), extra={'methodname': self.__class__.__name__})
 
     def requires(self):
         return AnalyzerTask(run_date=self.run_date, index=self.index, doc_type=self.doc_type)
